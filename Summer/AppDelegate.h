191791764a4c31326f0de64e74f072d0f6a565b4
Jonathan Hirz <jonathan@Jonathans-MacBook-Air.local>
2012-02-22 13:10:57 -0800

    Initial Commit

---
+++ Summer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Summer
+//
+//  Created by Jonathan Hirz on 2/22/12.
+//  Copyright (c) 2012 SuaveApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
