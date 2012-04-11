56025a7e5fd8dc4d7825752ec80bd9ae45c56f5c
Jonathan <jonathanhirz@gmail.com>
2012-04-10 19:30:12 -0700

    initial
    Resume for shotgun applications to Apple

---
+++ Summer/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Summer
+//
+//  Created by Jonathan Hirz on 2/25/12.
+//  Copyright (c) 2012 SuaveApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Summer/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  Summer
+//
+//  Created by Jonathan Hirz on 2/28/12.
+//  Copyright (c) 2012 SuaveApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
